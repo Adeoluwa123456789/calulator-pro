c28a526a75cd571d3110c7523cbd79557a3723fe
Adeoluwa123456789 <fatorisaadeoluwa@gmail.com>
2024-01-31 11:11:05 +0100

    i added 1 file

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+function every ( val) {
+    document.getElementById('display').value +=val
+     }
+    
+     function equal() {
+        try {
+            document.getElementById('display').value = eval(document.getElementById ('display').value);
+        } catch (error) {
+            document.getElementById('display').value='';
+        }
+    }
+
+    function delall(){
+    if (delall !== true) {
+        document.getElementById('display').value =''
+        
+    }
+      
+    }
+    function splice(){
+        if (splice !== true) {
+            document.getElementById('display').value =''
+            
+        }
+          
+        }
+        // window.close();
+        function togglePower() {
+            if (togglePower !== true ) {
+                container.style.display = 'block' 
+            }
+                    
+        } 
+        function togglePower2() {
+            if (togglePower !== true) {
+                container.style.display= 'none'
+            }
+        }
